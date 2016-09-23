4f9c07717e06b39c3f9aa610d4f926c36a41484e
CarolMadeleine <karitomadeleine@gmail.com>
2016-09-23 15:53:44 -0500

    subiendo logos y mapas

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function(){
+
+});
+
+document.getElementById("mapa").addEventListener("scroll", myFunction);
+
+function myFunction() {
+    document.getElementById("mapa").appendChild= "mapa";
+}
+
+
+
+
+
+
+
